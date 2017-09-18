23c91f57b0260401f2bca28e8855f0e1230531d1
Toluwase <wase932@gmail.com>
2017-09-18 05:34:12 -0700

    Two-way event Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    allowToAddServer = false;
+    serverCreationStatus = 'No server was created';
+    serverName = '';
     sampleVar: String = 'This is from the Server Component';
+    constructor() {
+    setTimeout(() => {this.allowToAddServer = !this.allowToAddServer; } , 3000);
+    }
+
+    ngOnInit() {
+        // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        // Add 'implements OnInit' to the class.
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'server was created';
+    }
+
+    onUpdateServer(event: Event) {
+        this.serverName = (<HTMLInputElement> event.target).value;
+        // console.log(event);
+
+    }
 }
+
+
