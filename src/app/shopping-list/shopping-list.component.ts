579129c52e079fdb82739d8f887d7239d35d9fe0
Toluwase <wase932@gmail.com>
2017-09-22 23:07:00 -0700

    Angular LifeCycle Hooks

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,3 +20,44 @@ export class ShoppingListComponent implements OnInit {
   }
 
 }
+// Angular LifeCycle Hooks:
+/*
+ngOnChanges(changes: SimpleChanges) {
+  //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+  //Add '${implements OnChanges}' to the class.
+}
+
+ngOnInit() {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+}
+
+ngDoCheck() {
+  //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+  //Add 'implements DoCheck' to the class.
+}
+
+ngAfterContentChecked() {
+  //Called after every check of the component's or directive's content.
+  //Add 'implements AfterContentChecked' to the class.
+  
+}
+
+ngAfterViewInit() {
+  //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+  //Add 'implements AfterViewInit' to the class.
+  
+}
+
+ngAfterViewChecked() {
+  //Called after every check of the component's view. Applies to components only.
+  //Add 'implements AfterViewChecked' to the class.
+  
+}
+
+ngOnDestroy() {
+  //Called once, before the instance is destroyed.
+  //Add 'implements OnDestroy' to the class.
+  
+}
+*/
