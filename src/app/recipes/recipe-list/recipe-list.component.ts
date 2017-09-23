93ffcf2aefa3ed77a3eaa0b2236fefd30c3305da
Toluwase <wase932@gmail.com>
2017-09-23 09:56:21 -0700

    Life Cycle Hooks: NgDoCheck

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,12 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, DoCheck {
   Recipes: Recipe[];
   constructor() { }
 
@@ -17,6 +17,12 @@ export class RecipeListComponent implements OnInit {
       , 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9RtVx0juYB6U5B2vUhK3a7Xb9Z0I2PSzsphu6LdZC7RWoE1kbTA')];
 
       console.log('Ng On Init Called');
+  }
+
+  ngDoCheck() {
+    // Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    // Add 'implements DoCheck' to the class.
+    console.log('DoCheck was called'); 
   }
 
   addRecipe() {
