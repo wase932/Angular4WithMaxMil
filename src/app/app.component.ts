808c6a5badf53fc2a76554a27c48d6a298544725
Toluwase <wase932@gmail.com>
2017-09-23 22:54:24 -0700

    Assignment Bonus: Parent - Child Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- receivedInterval = 0;
  parentInterval: number;
  OddNumbers: number[] = [];
  EvenNumbers: number[] = [];
