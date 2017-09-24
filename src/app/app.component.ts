9872789736203b3671ffd311b0770855b01b5648
Toluwase <wase932@gmail.com>
2017-09-23 23:47:39 -0700

    Meun Item Navigation using custom event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentButton: string;
+
+  onMenuEventReceived(event) {
+    this.currentButton = event;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  buttonClicked = 'recipes';
+  @Output() fireButtonClick = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBtnClick(event) {
+    this.fireButtonClick.emit(event);
+    this.buttonClicked = event;
+    console.log(event);
   }
 
 }
