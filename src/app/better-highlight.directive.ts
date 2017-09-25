32a171f30da1b8c546d35d661b90978bdb54c5a6
Toluwase <wase932@gmail.com>
2017-09-24 23:37:00 -0700

    Binding to DIrective Properties using default property name

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,22 +1,23 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'yellow';
   constructor( private elref: ElementRef, private renderer: Renderer2) {
    }
 
    @HostListener('mouseenter') mouseOver() {
-     this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'grey');
+     this.renderer.setStyle(this.elref.nativeElement, 'background-color', this.highlightColor);
    }
 
    @HostListener('mouseleave') mouseLeave() {
-    this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'yellow');
+    this.renderer.setStyle(this.elref.nativeElement, 'background-color', this.defaultColor);
   }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'yellow');
+    this.renderer.setStyle(this.elref.nativeElement, 'background-color', this.defaultColor);
   }
 }
