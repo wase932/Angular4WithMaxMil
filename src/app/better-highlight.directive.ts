851d7b89efd3d8062b15479c38929f58aa8db420
Toluwase <wase932@gmail.com>
2017-09-24 22:43:50 -0700

    Using Host Listener to listen to Host Events for custom Attribute directive

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
 
-  constructor() { }
+  constructor( private elref: ElementRef, private renderer: Renderer2) {
+   }
 
+   @HostListener('mouseenter') mouseOver() {
+     this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'grey');
+   }
+
+   @HostListener('mouseleave') mouseLeave() {
+    this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'yellow');
+  }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elref.nativeElement, 'background-color', 'yellow');
+  }
 }
