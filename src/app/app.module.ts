6d95dc4a0183ec4e07bc632b4058fe5035bb4590
Toluwase <wase932@gmail.com>
2017-09-26 05:15:02 -0700

    Clean App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
-import { AssignmentDirectivesComponent } from './assignment-directives/assignment-directives.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { HeaderComponent } from './header/header.component';
-import { OpenDropDownDirective } from './open-drop-down.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    WarningComponent,
-    SuccessComponent,
-    AssignmentDirectivesComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    HeaderComponent,
-    OpenDropDownDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
