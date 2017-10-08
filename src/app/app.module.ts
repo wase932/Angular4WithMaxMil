e6f8e8aae259c690e73286c7ef8db0a778fb4e6f
Toluwase <wase932@gmail.com>
2017-10-07 23:52:34 -0700

    Routing Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './users/user/user.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ServersComponent,
+    HomeComponent,
+    UserComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
