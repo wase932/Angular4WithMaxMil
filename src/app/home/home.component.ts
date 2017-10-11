32f551c46765cf468e2595aca698f5a56bf8025e
Toluwase <wase932@gmail.com>
2017-10-10 21:04:58 -0700

    Programatic Navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public countDown = 5;
+  private interval;
+  constructor (private route: Router) {
+  }
 
   ngOnInit() {
   }
 
+  onAutomaticRoute() {
+   this.interval = setInterval(() => {
+
+      if (this.countDown > 0) {
+     this.countDown --; }
+
+     if (this.countDown === 0) {
+      this.route.navigate(['/users']);
+      clearInterval(this.interval);
+    }
+    }
+    , 1000);
+  }
 }
