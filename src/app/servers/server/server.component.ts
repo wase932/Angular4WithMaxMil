1ad7e3ed3e7b1b92e334d60f4cabf2867a8a08d0
Toluwase <wase932@gmail.com>
2017-10-15 21:23:49 -0700

    Gotcha: => Received route reactively. Casting Param values to desired type.

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -9,11 +9,23 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
-
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) {
+   }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // To receive initial value:
+    /* Note that a "+" is added to cast the value as an int (number);
+     * To test: Goto: http://localhost:4200/servers/1 (availabe servers 1 to 3)
+     */
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+
+    // To react to subsequent changes in the router parameters:
+    // Again note the cast to int.
+    // Without the cast, the parameter passed will be a string, and would cause an exception to the getServer(value: number) method
+    this.route.params.subscribe( (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+     });
   }
 
 }
